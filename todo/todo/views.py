8d596a85b685d9f7063e9ef2d48122077e000ae0
VrishaanganVS <teknobot24@gmail.com>
2025-04-06 16:44:33 +0530

    Completion of the signup page

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,4 +1,7 @@
 from django . shortcuts import render,redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import todoo
 
 def signup(request):
     if request.method == 'POST':
@@ -6,6 +9,10 @@ def signup(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         print(username,email,password)
+        
+        my_user = User.objects.create(username,email,password)
+        my_user.save()
+        return redirect('login.html')
     return render(request, 'signup.html')
 
 def login(request):
